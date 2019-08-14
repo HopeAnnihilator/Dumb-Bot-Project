16c7039fbc83b9a4e8759ff65c59d7f8e106e3de
HopeAnnihilator <36973966+HopeAnnihilator@users.noreply.github.com>
2019-08-14 15:26:33 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,49 +1,59 @@
 import discord
 from discord.ext import commands
 import time
+import threading
+
+global holons
+global opted_out
+
 
 client = commands.Bot(command_prefix = '')
 opted_out = open("opted_out.txt").read().splitlines()
 holons = []
 
+            
 @client.event
 async def on_ready():
     print('The Damned Thing Actually Started')
+    timer = threading.Timer(30, save)
+    timer.start()
 
-            
+
+def save():
+    with open('opted_out.txt', 'w') as yay:
+        for user in opted_out:
+            if user:
+                yay.write(user + '\n')
+    print('opted_out saved')
+    timer = threading.Timer(30, save)
+    timer.start()
+    
+
 @client.event
 async def on_message(message):
-    global opted_out
-    global holons
+
     if message.content.startswith('Hocus Pocus'): #this is where commands are run to bot by beginning message with 'Hocus Pocus'
         if "opt out" in message.content: 
             if str(message.author.id) in opted_out:
                 await message.channel.send(str(message.author) + ' is already opted out')
-            else: #gotta love this shitshow
-                opted_out = open("opted_out.txt", 'a')
-                opted_out.write(str(message.author.id) + '\n')
-                opted_out.close()
-                opted_out = open("opted_out.txt").read().splitlines()
+            else:
+                opted_out.append(str(message.author.id))
                 await message.channel.send(str(message.author) + ' is now opted out')
                 
         elif "opt in" in message.content:
             if str(message.author.id) in opted_out: 
-                with open('opted_out.txt', 'w') as f: #gotta love this shitshow
-                    for line in opted_out:
-                        if line != str(message.author.id) and line:
-                            f.write(line + '\n')
-                opted_out = open("opted_out.txt").read().splitlines()
+                opted_out.remove(str(message.author.id))
                 await message.channel.send(str(message.author) + ' is now opted in')
             else:
                 await message.channel.send(str(message.author) + ' is not opted out')
                 
         elif "list" in message.content: #this is made so that multiple lists can be output at once
-            if str(message.author.id) == user_id and 'opted_out' in message.content:
+            if str(message.author.id) == admin_user_id and 'opted_out' in message.content:
                 if opted_out:
                     await message.channel.send('**opted_out:**')
                     await message.channel.send(opted_out)
                 else:
-                    await message.channel.send('**Either your list is fucked or nobody opted out!**')
+                    await message.channel.send('***Either your list is fucked or nobody opted out!***')
             if str(message.author.id) == user_id and 'holons' in message.content:
                 if holons:
                     await message.channel.send('**holons:**')
@@ -66,7 +76,7 @@ async def on_message(message):
             wordlist.write(message.clean_content + " ")
             print(message.content)  
 
-user_id = ''
+admin_user_id = ''
 bot_id = ''
 token = ''
 client.run(token)
